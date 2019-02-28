ea8d43401f344e35cceb262ad0dbda55953bfb80
barakbarclay <barakbarclay@gmail.com>
2019-02-27 20:16:34 -0700

    HW5 Part 1 Finished (Finds items in a list that are not in another list.)

---
+++ CS3020_HW5_P1/CS3020_HW5_P1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS3020_HW5_P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listA = new List<string>() {"aaa", "bbb", "ccc", "ddd" };
+            List<string> listB = new List<string>() { "aaa", "ccc", "eee" };
+            List<string> listC = new List<string>();
+
+            //Finding strings in listA that are not in listB
+            //Via LINQ
+            Console.WriteLine("Via Linq:");
+            listC = listA.Except(listB).ToList();
+            Console.WriteLine(String.Join("\n", listC));
+
+            listC.Clear();
+
+            //Via Loops and If statments
+            Console.WriteLine("Via Loops:");
+            int exists = 0;
+            for (int i = 0; i < listA.Count; i++)
+            {
+                exists = 0;
+                for (int j = 0; j < listB.Count; j++)
+                {
+                    if (listB[j] == listA[i])
+                    {
+                        exists = 1;
+                    }
+                }
+                if (exists == 0)
+                {
+                    listC.Add(listA[i]);
+                }
+            }
+            for (int i = 0; i < listC.Count; i++)
+            {
+                Console.WriteLine(listC[i]);
+            }
+        }
+    }
+}
